a27b428f1c7ce9b3eb0c565017c13e317374b3cf
Senkevich Kirill <sks89@mail.ru>
2019-04-24 00:33:38 +0300

    gmap library replaced by the new one

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,62 +1,51 @@
 import React from 'react';
-import {withGoogleMap, withHandlers, GoogleMap, Marker} from "react-google-maps"
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 class MainMap extends React.Component {
+
+    map = null;
 
     constructor(props) {
         super(props);
     }
 
-
     panMapToCoordinates(coords) {
-        this._map.setCenter(coords);
+        this.map.panTo(coords);
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
-        const lastMarker = nextProps.markers.pop();
-        console.log(lastMarker);
-        this.panMapToCoordinates(lastMarker);
+        console.log('componentWillReceiveProps', this.props);
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        // console.log(prevProps);
-    }
-
-    handleMapMounted = (map) => {
-        this._map = map;
-        console.log(this._map.getCenter() );
-    };
-
-    getGoogleMapContainer() {
-
-        return withGoogleMap((props) =>
-            <GoogleMap
-                defaultZoom={8}
-                ref={props.onMapMounted}
-                loadingElement={<div style={{ height: `100%` }} />}
-                onMapMounted={this.handleMapMounted}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-                defaultCenter={{ lat: -34.397, lng: 150.644 }} >
-
-                {this.props.markers.map((marker, index) => (
-                    <Marker key={index.toString()} position={marker} />
-                ))}
-            </GoogleMap>
-        )
+        const lastMarker = this.props.markers[this.props.markers.length - 1];
+        this.panMapToCoordinates(lastMarker);
     }
 
     render() {
 
-        const GmapContainer = this.getGoogleMapContainer();
-
         return (
-            <GmapContainer
-                loadingElement={<div style={{ height: `100%` }} />}
-                onMapMounted={this.handleMapMounted}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
+            <LoadScript id="script-loader" googleMapsApiKey="AIzaSyAaE9Ofwn52HHf_6xyeHB_3rUY_Usb7YAs" {...this.props}>
+                <GoogleMap
+                    id='main-map'
+                    zoom={8}
+                    onLoad={map => {
+                        this.map = map;
+                    }}
+                    mapContainerStyle={{
+                        height: "400px",
+                        width: "100%"
+                    }}
+                    center={{
+                        lat: -3.745,
+                        lng: -38.523
+                    }}
+                >
+                    {this.props.markers.map((marker, index) => (
+                        <Marker key={index.toString()} position={marker} />
+                    ))}
+                </GoogleMap>
+            </LoadScript>
         )
     }
 
